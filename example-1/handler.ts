7eebb3ca686ee2aba3598fb6c913528503273a98
unperknown <unperknown@icloud.com>
2020-09-02 13:25:18 +0900

    update: refactored lambda function

--- example-1/handler.ts
+++ example-1/handler.ts
@@ -1,52 +1,23 @@
-import { v4 as uuidv4 } from "uuid";
-import AWS, { AWSError } from "aws-sdk";
-import { APIGatewayEvent, Context, Callback } from "aws-lambda";
-import { PutItemOutput } from "aws-sdk/clients/dynamodb";
+import { v4 as uuidv4 } from 'uuid';
+import { APIGatewayEvent } from 'aws-lambda';
+import { response } from './middlewares';
+import { user } from './models';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-const main = (event: APIGatewayEvent, context: Context, callback: Callback) => {
-  // Fetch request body
+const main = response(async (event: APIGatewayEvent) => {
   const data = JSON.parse(event.body);
-
-  // Sample row
-  const inputs = {
-    TableName: "test",
+  const input = {
+    TableName: 'test',
     Item: {
-      id: event.requestContext.identity.cognitoIdentityId,
-      uid: uuidv4(),
+      id: uuidv4(),
+      uid: event.requestContext.identity.cognitoIdentityId,
       user: data.user,
       age: data.age,
     },
   };
 
-  dynamoDB.put(inputs, (err: AWSError, data: PutItemOutput) => {
-    // enable CORS
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-    };
+  await user.put(input);
 
-    if (err) {
-      const response = {
-        statusCode: 500,
-        headers: headers,
-        body: JSON.stringify({ status: false }),
-      };
-
-      callback(null, response);
-
-      return;
-    }
-
-    const repsonse = {
-      statusCode: 200,
-      headers: headers,
-      body: JSON.stringify(inputs.Item),
-    };
-
-    callback(null, repsonse);
-  });
-};
+  return input.Item;
+});
 
 export { main };
